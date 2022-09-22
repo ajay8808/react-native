0c961318cf8112fc09bad051a1af3bfe75b38f07
ajay8808 <48876700+ajay8808@users.noreply.github.com>
2022-09-22 22:11:19 +0900

    width height 컴포넌트

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -16,6 +16,7 @@ import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
+import HeightWidth from '../screens/heightWidth';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -86,12 +87,13 @@ function BottomTabNavigator() {
       />
       <BottomTab.Screen
         name="TabTwo"
-        component={TabTwoScreen}
+        component={HeightWidth}
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+     
     </BottomTab.Navigator>
   );
 }
